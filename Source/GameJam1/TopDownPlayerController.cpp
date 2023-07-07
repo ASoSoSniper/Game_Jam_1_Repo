e6857a5317b1c02cbedec168befd7f0c5e35474d
Benjamin Smith <bsmithnh2@gmail.com>
2023-07-07 19:44:07 -0400

    Player Rotation

--- Source/GameJam1/TopDownPlayerController.cpp
+++ Source/GameJam1/TopDownPlayerController.cpp
@@ -4,6 +4,7 @@
 #include "TopDownPlayerController.h"
 #include "Camera/CameraActor.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 
 ATopDownPlayerController::ATopDownPlayerController()
 {
@@ -14,5 +15,28 @@ void ATopDownPlayerController::BeginPlay()
 {
 	camera = Cast<ACameraActor>(UGameplayStatics::GetActorOfClass(GetWorld(), ACameraActor::StaticClass()));
 	SetViewTarget(camera);
-	
+	player = Cast<APlayerCharacter>(UGameplayStatics::GetActorOfClass(GetWorld(), APlayerCharacter::StaticClass()));
+}
+
+void ATopDownPlayerController::Tick(float DeltaTime)
+{
+	RotatePlayer(DeltaTime);
+}
+
+void ATopDownPlayerController::RotatePlayer(float& DeltaTime)
+{
+	FVector mousePosition, mouseDirection;
+	const bool bSuccess = DeprojectMousePositionToWorld(mousePosition, mouseDirection);
+
+	if (bSuccess && player)
+	{
+		FVector playerLocation = player->GetActorLocation();
+		FVector endLocation = FMath::LinePlaneIntersection(mousePosition, mousePosition + (mouseDirection * 2000.f), playerLocation, FVector(0, 0, 1));
+
+		FRotator newRotation = UKismetMathLibrary::FindLookAtRotation(player->GetActorLocation(), endLocation);
+		FRotator playerRotation = player->GetActorRotation();
+		FRotator targetRot = FRotator(playerRotation.Pitch, newRotation.Yaw, playerRotation.Roll);
+		//FRotator step = FMath::RInterpTo(playerRotation, targetRot, DeltaTime, 50.f);
+		player->SetActorRotation(targetRot);
+	}
 }
--- Source/GameJam1/TopDownPlayerController.h
+++ Source/GameJam1/TopDownPlayerController.h
@@ -18,8 +18,10 @@ class GAMEJAM1_API ATopDownPlayerController : public APlayerController
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 public:
 	APlayerCharacter* player;
 	ACameraActor* camera;
 
+	void RotatePlayer(float& DeltaTime);
 };
